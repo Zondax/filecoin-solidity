2d2a3d4a0d89daff4f13410ff7f2bae3b6f6124d
Emmanuel <emmanuelm41@gmail.com>
2022-11-23 15:50:25 -0300

    add new task on hardhat to test miner method

--- hardhat/tasks/index.js
+++ hardhat/tasks/index.js
@@ -1,3 +1,5 @@
 exports.getBalance = require("./simple-coin/get-balance")
 exports.getAddress = require("./get-address")
 exports.sendCoin = require("./simple-coin/send-coin")
+
+exports.changeBeneficiary = require("./miner-api/change-beneficiary")
