c5f48f9331d75d9fa70030db984e7947a01e0f11
Emmanuel <emmanuelm41@gmail.com>
2022-11-25 14:46:21 -0300

    feat: serialize and deserialize functions for multiple methods

--- contracts/v0.8/MinerAPI.sol
+++ contracts/v0.8/MinerAPI.sol
@@ -7,66 +7,75 @@ import "./cbor/MinerCbor.sol";
 /// @title This contract is a proxy to a built-in Miner actor. Calling one of its methods will result in a cross-actor call being performed
 /// @author Zondax AG
 contract MinerAPI {
-    using ChangeBeneficiaryParamsCBOR for MinerTypes.ChangeBeneficiaryParams;
+    using ChangeBeneficiaryCBOR for MinerTypes.ChangeBeneficiaryParams;
+    using GetOwnerCBOR for MinerTypes.GetOwnerReturn;
+    using AddressCBOR for bytes;
+    using IsControllingAddressCBOR for MinerTypes.IsControllingAddressReturn;
+    using GetSectorSizeCBOR for MinerTypes.GetSectorSizeReturn;
+    using GetAvailableBalanceCBOR for MinerTypes.GetAvailableBalanceReturn;
 
     /// @notice Income and returned collateral are paid to this address
     /// @notice This address is also allowed to change the worker address for the miner
     /// @return the owner address of a Miner
-    function get_owner()
-        public
-        view
-        returns (MinerTypes.GetOwnerReturn memory)
-    {
+    function get_owner() public view returns (MinerTypes.GetOwnerReturn memory) {
         // FIXME make actual call to the miner actor
-        bytes memory owner = hex"010101";
+        bytes memory raw_response = hex"81480111010213123122";
 
-        return MinerTypes.GetOwnerReturn(owner);
+        MinerTypes.GetOwnerReturn memory response;
+        response.deserialize(raw_response);
+
+        return response;
     }
 
     /// @param addr New owner address
     /// @notice Proposes or confirms a change of owner address.
     /// @notice If invoked by the current owner, proposes a new owner address for confirmation. If the proposed address is the current owner address, revokes any existing proposal that proposed address.
     function change_owner_address(bytes memory addr) public {
+        bytes memory raw_request = addr.serializeAddress();
+
         // FIXME make actual call to the miner actor
     }
 
     /// @param addr The "controlling" addresses are the Owner, the Worker, and all Control Addresses.
     /// @return Whether the provided address is "controlling".
-    function is_controlling_address(
-        bytes memory addr
-    ) public pure returns (MinerTypes.IsControllingAddressReturn memory) {
+    function is_controlling_address(bytes memory addr) public pure returns (MinerTypes.IsControllingAddressReturn memory) {
+        bytes memory raw_request = addr.serializeAddress();
+
         // FIXME make actual call to the miner actor
-        return MinerTypes.IsControllingAddressReturn(false);
+        bytes memory raw_response = hex"81F5";
+
+        MinerTypes.IsControllingAddressReturn memory response;
+        response.deserialize(raw_response);
+
+        return response;
     }
 
     /// @return the miner's sector size.
-    function get_sector_size()
-        public
-        view
-        returns (MinerTypes.GetSectorSizeReturn memory)
-    {
+    function get_sector_size() public view returns (MinerTypes.GetSectorSizeReturn memory) {
         // FIXME make actual call to the miner actor
-        return MinerTypes.GetSectorSizeReturn(1);
+        bytes memory raw_response = hex"811A0756B5B3";
+
+        MinerTypes.GetSectorSizeReturn memory response;
+        response.deserialize(raw_response);
+
+        return response;
     }
 
     /// @notice This is calculated as actor balance - (vesting funds + pre-commit deposit + initial pledge requirement + fee debt)
     /// @notice Can go negative if the miner is in IP debt.
     /// @return the available balance of this miner.
-    function get_available_balance()
-        public
-        pure
-        returns (MinerTypes.GetAvailableBalanceReturn memory)
-    {
+    function get_available_balance() public pure returns (MinerTypes.GetAvailableBalanceReturn memory) {
         // FIXME make actual call to the miner actor
-        return MinerTypes.GetAvailableBalanceReturn(10000000000000000000000);
+        bytes memory raw_response = hex"813B00006FFF85376C48";
+
+        MinerTypes.GetAvailableBalanceReturn memory response;
+        response.deserialize(raw_response);
+
+        return response;
     }
 
     /// @return the funds vesting in this miner as a list of (vesting_epoch, vesting_amount) tuples.
-    function get_vesting_funds()
-        public
-        pure
-        returns (MinerTypes.GetVestingFundsReturn memory)
-    {
+    function get_vesting_funds() public pure returns (MinerTypes.GetVestingFundsReturn memory) {
         // FIXME make actual call to the miner actor
 
         CommonTypes.VestingFunds[] memory vesting_funds;
@@ -77,9 +86,7 @@ contract MinerAPI {
     /// @notice Proposes or confirms a change of beneficiary address.
     /// @notice A proposal must be submitted by the owner, and takes effect after approval of both the proposed beneficiary and current beneficiary, if applicable, any current beneficiary that has time and quota remaining.
     /// @notice See FIP-0029, https://github.com/filecoin-project/FIPs/blob/master/FIPS/fip-0029.md
-    function change_beneficiary(
-        MinerTypes.ChangeBeneficiaryParams memory params
-    ) public {
+    function change_beneficiary(MinerTypes.ChangeBeneficiaryParams memory params) public {
         bytes memory raw_request = params.serialize();
 
         // FIXME make actual call to the miner actor
@@ -87,11 +94,7 @@ contract MinerAPI {
 
     /// @notice This method is for use by other actors (such as those acting as beneficiaries), and to abstract the state representation for clients.
     /// @notice Retrieves the currently active and proposed beneficiary information.
-    function get_beneficiary()
-        public
-        view
-        returns (MinerTypes.GetBeneficiaryReturn memory)
-    {
+    function get_beneficiary() public view returns (MinerTypes.GetBeneficiaryReturn memory) {
         // FIXME make actual call to the miner actor
 
         CommonTypes.ActiveBeneficiary memory activeBeneficiary;
--- contracts/v0.8/cbor/MinerCbor.sol
+++ contracts/v0.8/cbor/MinerCbor.sol
@@ -4,11 +4,13 @@ pragma solidity >=0.4.25 <=0.8.17;
 import "solidity-cborutils/contracts/CBOR.sol";
 
 import {MinerTypes} from "../types/MinerTypes.sol";
+import "../utils/CborDecode.sol";
 
 /// @title FIXME
 /// @author Zondax AG
-library ChangeBeneficiaryParamsCBOR {
+library ChangeBeneficiaryCBOR {
     using CBOR for CBOR.CBORBuffer;
+    using CBORDecoder for bytes;
 
     function serialize(MinerTypes.ChangeBeneficiaryParams memory params) internal pure returns (bytes memory) {
         // FIXME what should the max length be on the buffer?
@@ -20,5 +22,101 @@ library ChangeBeneficiaryParamsCBOR {
         buf.writeUInt64(params.new_expiration);
 
         return buf.data();
+    }
+}
+
+library GetOwnerCBOR {
+    using CBOR for CBOR.CBORBuffer;
+    using CBORDecoder for bytes;
+
+    function deserialize(MinerTypes.GetOwnerReturn memory ret, bytes memory rawResp) internal pure {
+        bytes memory owner;
+        uint byteIdx = 0;
+        uint len;
+
+        (len, byteIdx) = rawResp.readFixedArray(byteIdx);
+        assert(len == 1);
+
+        (owner, byteIdx) = rawResp.readBytes(byteIdx);
+
+        ret.owner = owner;
+    }
+}
+
+library IsControllingAddressCBOR {
+    using CBOR for CBOR.CBORBuffer;
+    using CBORDecoder for bytes;
+
+    function deserialize(MinerTypes.IsControllingAddressReturn memory ret, bytes memory rawResp) internal pure {
+        bool is_controlling;
+        uint byteIdx = 0;
+        uint len;
+
+        (len, byteIdx) = rawResp.readFixedArray(byteIdx);
+        assert(len == 1);
+
+        (is_controlling, byteIdx) = rawResp.readBool(byteIdx);
+
+        ret.is_controlling = is_controlling;
+    }
+}
+
+library GetSectorSizeCBOR {
+    using CBOR for CBOR.CBORBuffer;
+    using CBORDecoder for bytes;
+
+    function deserialize(MinerTypes.GetSectorSizeReturn memory ret, bytes memory rawResp) internal pure {
+        uint64 sector_size;
+        uint byteIdx = 0;
+        uint len;
+
+        (len, byteIdx) = rawResp.readFixedArray(byteIdx);
+        assert(len == 1);
+
+        (sector_size, byteIdx) = rawResp.readUInt64(byteIdx);
+
+        ret.sector_size = sector_size;
+    }
+}
+
+library GetAvailableBalanceCBOR {
+    using CBOR for CBOR.CBORBuffer;
+    using CBORDecoder for bytes;
+
+    function deserialize(MinerTypes.GetAvailableBalanceReturn memory ret, bytes memory rawResp) internal pure {
+        int256 available_balance;
+        uint byteIdx = 0;
+        uint len;
+
+        (len, byteIdx) = rawResp.readFixedArray(byteIdx);
+        assert(len == 1);
+
+        (available_balance, byteIdx) = rawResp.readInt256(byteIdx);
+
+        ret.available_balance = available_balance;
+    }
+}
+
+library AddressCBOR {
+    using CBOR for CBOR.CBORBuffer;
+    using CBORDecoder for bytes;
+
+    function serializeAddress(bytes memory addr) internal pure returns (bytes memory) {
+        // FIXME what should the max length be on the buffer?
+        CBOR.CBORBuffer memory buf = CBOR.create(64);
+
+        buf.writeBytes(addr);
+
+        return buf.data();
+    }
+
+    function deserializeAddress(bytes memory ret) internal pure returns (bytes memory) {
+        bytes memory addr;
+        uint byteIdx = 0;
+        uint len;
+
+        (addr, byteIdx) = ret.readBytes(byteIdx);
+
+        return addr;
     }
 }
