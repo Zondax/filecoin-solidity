b56a96ec2255a469963879b6a3480df99cfc436d
Emmanuel <emmanuelm41@gmail.com>
2022-11-28 14:55:51 -0300

    add missing build command for verifreg actor

--- contracts/v0.8/VerifRegAPI.sol
+++ contracts/v0.8/VerifRegAPI.sol
@@ -14,8 +14,31 @@ contract VerifRegAPI {
     function get_claims(VerifRegTypes.GetClaimsParams memory params) public view returns (VerifRegTypes.GetClaimsReturn memory) {
         bytes memory raw_request = params.serialize();
 
-        // FIXME make actual call to the actor
-        bytes memory raw_response = hex"828200838201018203038202028288010245010101010101020304058801024501010101010102030405";
+        // FIXME: find the method num
+        uint64 method_num = 0x00;
+
+        // FIXME: unknown size for the response
+        bytes memory raw_response = new bytes(0x0100);
+
+        assembly {
+            let request := mload(add(raw_request, 0x20))
+            let input := mload(0x40)
+            mstore(input, method_num)
+            mstore(add(input, 0x20), CODEC)
+            // address size
+            mstore(add(input, 0x40), 0x02)
+            // params size
+            mstore(add(input, 0x60), mload(raw_request))
+            // actual params
+            mstore(add(input, 0x80), request)
+            // actual address
+            mstore(add(input, 0xa0), hex"0066")
+            // no params
+            // staticcall(gasLimit, to, inputOffset, inputSize, outputOffset, outputSize)
+            if iszero(staticcall(100000000, 0x0e, input, 0x0100, raw_response, 0x0100)) {
+                revert(0, 0)
+            }
+        }
 
         VerifRegTypes.GetClaimsReturn memory response;
         response.deserialize(raw_response);
@@ -26,6 +49,32 @@ contract VerifRegAPI {
     function add_verified_client(VerifRegTypes.AddVerifierClientParams memory params) public view {
         bytes memory raw_request = params.serialize();
 
-        // FIXME make actual call to the actor
+        // FIXME: find the method num
+        uint64 method_num = 0x00;
+
+        // FIXME: unknown size for the response
+        bytes memory raw_response = new bytes(0x0100);
+
+        assembly {
+            let request := mload(add(raw_request, 0x20))
+            let input := mload(0x40)
+            mstore(input, method_num)
+            mstore(add(input, 0x20), CODEC)
+            // address size
+            mstore(add(input, 0x40), 0x02)
+            // params size
+            mstore(add(input, 0x60), mload(raw_request))
+            // actual params
+            mstore(add(input, 0x80), request)
+            // actual address
+            mstore(add(input, 0xa0), hex"0066")
+            // no params
+            // staticcall(gasLimit, to, inputOffset, inputSize, outputOffset, outputSize)
+            if iszero(staticcall(100000000, 0x0e, input, 0x0100, raw_response, 0x0100)) {
+                revert(0, 0)
+            }
+        }
+
+        return;
     }
 }
