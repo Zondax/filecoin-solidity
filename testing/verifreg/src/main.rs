c36d9ac9e7f01a76e8a6e443e80e135dbc01ef4b
Emmanuel <emmanuelm41@gmail.com>
2022-11-29 15:49:13 -0300

    add base environment for test power and verifreg actors

---
+++ testing/verifreg/src/main.rs
@@ -0,0 +1,85 @@
+use fvm_integration_tests::tester::{Account, Tester};
+use fvm_integration_tests::dummy::DummyExterns;
+use fvm_integration_tests::bundle;
+use fvm_ipld_encoding::{strict_bytes, BytesDe, tuple::*};
+use fvm_shared::state::StateTreeVersion;
+use fvm_shared::version::NetworkVersion;
+use fvm_ipld_blockstore::MemoryBlockstore;
+use fvm_shared::address::Address;
+use std::env;
+use fvm_shared::message::Message;
+use fvm::executor::{ApplyKind, Executor};
+use fil_actor_eam::Return;
+use fil_actor_init::ExecReturn;
+use fvm_ipld_encoding::RawBytes;
+use fil_actors_runtime::{EAM_ACTOR_ADDR, INIT_ACTOR_ADDR};
+use cid::Cid;
+use std::str::FromStr;
+use rand_core::OsRng;
+use bls_signatures::Serialize;
+use multihash::Code;
+use fvm_ipld_encoding::CborStore;
+use fvm::state_tree::{ActorState};
+use fvm_shared::econ::TokenAmount;
+
+const WASM_COMPILED_PATH: &str =
+   "../../build/v0.8/VerifRegAPI.bin";
+
+#[derive(Serialize_tuple, Deserialize_tuple)]
+pub struct Create2Params {
+    #[serde(with = "strict_bytes")]
+    pub initcode: Vec<u8>,
+    #[serde(with = "strict_bytes")]
+    pub salt: [u8; 32],
+}
+
+fn main() {
+    println!("Testing solidity API");
+
+    let bs = MemoryBlockstore::default();
+    let actors = std::fs::read("./builtin-actors-devnet-wasm.car").expect("Unable to read actor devnet file file");
+    let bundle_root = bundle::import_bundle(&bs, &actors).unwrap();
+
+    let mut tester =
+        Tester::new(NetworkVersion::V18, StateTreeVersion::V5, bundle_root, bs).unwrap();
+
+    let sender: [Account; 1] = tester.create_accounts().unwrap();
+
+    // Instantiate machine
+    tester.instantiate_machine(DummyExterns).unwrap();
+
+    let executor = tester.executor.as_mut().unwrap();
+
+    println!("Calling init actor (EVM)");
+
+    let wasm_path = env::current_dir()
+        .unwrap()
+        .join(WASM_COMPILED_PATH)
+        .canonicalize()
+        .unwrap();
+    let evm_hex = std::fs::read(wasm_path).expect("Unable to read file");
+    let evm_bin = hex::decode(evm_hex).unwrap();
+
+    let constructor_params = Create2Params {
+        initcode: evm_bin,
+        salt: [0; 32],
+    };
+
+    let message = Message {
+        from: sender[0].1,
+        to: EAM_ACTOR_ADDR,
+        gas_limit: 1000000000,
+        method_num: 3,
+        params: RawBytes::serialize(constructor_params).unwrap(),
+        ..Message::default()
+    };
+
+    let res = executor
+        .execute_message(message, ApplyKind::Explicit, 100)
+        .unwrap();
+
+    assert_eq!(res.msg_receipt.exit_code.value(), 0);
+
+    let exec_return : Return = RawBytes::deserialize(&res.msg_receipt.return_data).unwrap();
+
+}
