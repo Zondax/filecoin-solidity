a42c9b5a03069b38c684495a23cc46eb3b548c59
Emmanuel <emmanuelm41@gmail.com>
2022-11-29 16:51:10 -0300

    add remaining methods for verifreg actor

--- contracts/v0.8/types/VerifRegTypes.sol
+++ contracts/v0.8/types/VerifRegTypes.sol
@@ -18,4 +18,50 @@ library VerifRegTypes {
         bytes addr;
         uint256 allowance;
     }
+    struct RemoveExpiredAllocationsParams {
+        // Client for which to remove expired allocations.
+        uint64 client;
+        // Optional list of allocation IDs to attempt to remove.
+        // Empty means remove all eligible expired allocations.
+        uint64[] allocation_ids;
+    }
+    struct RemoveExpiredAllocationsReturn {
+        // Ids of the allocations that were either specified by the caller or discovered to be expired.
+        uint64[] considered;
+        // Results for each processed allocation.
+        CommonTypes.BatchReturn results;
+        // The amount of datacap reclaimed for the client.
+        int256 datacap_recovered;
+    }
+    struct RemoveExpiredClaimsParams {
+        // Provider to clean up (need not be the caller)
+        uint64 provider;
+        // Optional list of claim IDs to attempt to remove.
+        // Empty means remove all eligible expired claims.
+        uint64[] claim_ids;
+    }
+    struct RemoveExpiredClaimsReturn {
+        // Ids of the claims that were either specified by the caller or discovered to be expired.
+        uint64[] considered;
+        // Results for each processed claim.
+        CommonTypes.BatchReturn results;
+    }
+    struct ExtendClaimTermsParams {
+        CommonTypes.ClaimTerm[] terms;
+    }
+    struct UniversalReceiverParams {
+        /// Asset type
+        uint32 type_;
+        /// Payload corresponding to asset type
+        bytes payload;
+    }
+
+    struct AllocationsResponse {
+        // Result for each allocation request.
+        CommonTypes.BatchReturn allocation_results;
+        // Result for each extension request.
+        CommonTypes.BatchReturn extension_results;
+        // IDs of new allocations created.
+        uint64[] new_allocations;
+    }
 }
