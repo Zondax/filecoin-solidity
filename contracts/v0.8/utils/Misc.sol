191602d5b2922ff109719661d468ea62948a6a49
Emmanuel <emmanuelm41@gmail.com>
2022-11-29 14:42:28 -0300

    move copy function to misc library

--- contracts/v0.8/utils/Misc.sol
+++ contracts/v0.8/utils/Misc.sol
@@ -11,4 +11,25 @@ library Misc {
             mstore(add(b, 32), x)
         }
     }
+
+    function copy(uint src, uint dest, uint len) internal pure {
+        // Copy word-length chunks while possible
+        for (; len >= 32; len -= 32) {
+            assembly {
+                mstore(dest, mload(src))
+            }
+            dest += 32;
+            src += 32;
+        }
+
+        if (len == 0) return;
+
+        // Copy remaining bytes
+        uint mask = 256 ** (32 - len) - 1;
+        assembly {
+            let srcpart := and(mload(src), not(mask))
+            let destpart := and(mload(dest), mask)
+            mstore(dest, or(destpart, srcpart))
+        }
+    }
 }
