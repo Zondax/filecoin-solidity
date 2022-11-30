8eb511a1c01886befff0910e9d24b2d776f3e041
Emmanuel <emmanuelm41@gmail.com>
2022-11-30 12:49:51 -0300

    add get_owner test case and fix functionality

--- testing/miner/src/main.rs
+++ testing/miner/src/main.rs
@@ -220,4 +220,46 @@ fn main() {
     dbg!(&res);
 
     assert_eq!(res.msg_receipt.exit_code.value(), 0);
+
+    println!("Calling `get_owner`");
+
+    let message = Message {
+        from: sender[0].1,
+        to: Address::new_id(exec_return.actor_id),
+        gas_limit: 1000000000,
+        method_num: 2,
+        sequence: 5,
+        params: RawBytes::new(hex::decode("440AC298DC").unwrap()),
+        ..Message::default()
+    };
+
+    let res = executor
+        .execute_message(message, ApplyKind::Explicit, 100)
+        .unwrap();
+
+    dbg!(&res);
+
+    assert_eq!(res.msg_receipt.exit_code.value(), 0);
+
+
+/*
+    println!("Calling `is_controlling_address`");
+
+    let message = Message {
+        from: sender[0].1,
+        to: Address::new_id(exec_return.actor_id),
+        gas_limit: 1000000000,
+        method_num: 2,
+        sequence: 6,
+        params: RawBytes::new(hex::decode("5864BE3B757A000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000020066000000000000000000000000000000000000000000000000000000000000").unwrap()),
+        ..Message::default()
+    };
+
+    let res = executor
+        .execute_message(message, ApplyKind::Explicit, 100)
+        .unwrap();
+
+    dbg!(&res);
+
+    assert_eq!(res.msg_receipt.exit_code.value(), 0);*/
 }
