5f8821ca4a4b12c9fe27c27fff58b8904f940b6d
Emmanuel <emmanuelm41@gmail.com>
2022-12-01 11:12:23 -0300

    update verifreg actor lib with method numbers

--- contracts/v0.8/VerifRegAPI.sol
+++ contracts/v0.8/VerifRegAPI.sol
@@ -4,9 +4,6 @@ pragma solidity >=0.4.25 <=0.8.17;
 import "./types/VerifRegTypes.sol";
 import "./cbor/VerifRegCbor.sol";
 
-uint64 constant ADDRESS_MAX_LEN = 86;
-uint64 constant CODEC = 0x71;
-
 /// @title FIXME
 /// @author Zondax AG
 contract VerifRegAPI {
@@ -22,215 +19,83 @@ contract VerifRegAPI {
     using UniversalReceiverHookCBOR for VerifRegTypes.UniversalReceiverParams;
     using UniversalReceiverHookCBOR for VerifRegTypes.AllocationsResponse;
 
-    function get_claims(VerifRegTypes.GetClaimsParams memory params) public view returns (VerifRegTypes.GetClaimsReturn memory) {
+    function get_claims(VerifRegTypes.GetClaimsParams memory params) public returns (VerifRegTypes.GetClaimsReturn memory) {
         bytes memory raw_request = params.serialize();
 
-        // FIXME: find the method num
-        uint64 method_num = 0x00;
+        bytes memory raw_response = Misc.call_actor(2199871187, hex"0005", raw_request);
 
-        // FIXME: unknown size for the response
-        bytes memory raw_response = new bytes(0x0100);
-
-        assembly {
-            let request := mload(add(raw_request, 0x20))
-            let input := mload(0x40)
-            mstore(input, method_num)
-            mstore(add(input, 0x20), CODEC)
-            // address size
-            mstore(add(input, 0x40), 0x02)
-            // params size
-            mstore(add(input, 0x60), mload(raw_request))
-            // actual params
-            mstore(add(input, 0x80), request)
-            // actual address
-            mstore(add(input, 0xa0), hex"0066")
-            // no params
-            // staticcall(gasLimit, to, inputOffset, inputSize, outputOffset, outputSize)
-            if iszero(staticcall(100000000, 0x0e, input, 0x0100, raw_response, 0x0100)) {
-                revert(0, 0)
-            }
-        }
+        bytes memory result = Misc.getDataFromActorResponse(raw_response);
 
         VerifRegTypes.GetClaimsReturn memory response;
-        response.deserialize(raw_response);
+        response.deserialize(result);
 
         return response;
     }
 
-    function add_verified_client(VerifRegTypes.AddVerifierClientParams memory params) public view {
+    function add_verified_client(VerifRegTypes.AddVerifierClientParams memory params) public {
         bytes memory raw_request = params.serialize();
 
-        // FIXME: find the method num
-        uint64 method_num = 0x00;
+        bytes memory raw_response = Misc.call_actor(3916220144, hex"0005", raw_request);
 
-        // FIXME: unknown size for the response
-        bytes memory raw_response = new bytes(0x0100);
-
-        assembly {
-            let request := mload(add(raw_request, 0x20))
-            let input := mload(0x40)
-            mstore(input, method_num)
-            mstore(add(input, 0x20), CODEC)
-            // address size
-            mstore(add(input, 0x40), 0x02)
-            // params size
-            mstore(add(input, 0x60), mload(raw_request))
-            // actual params
-            mstore(add(input, 0x80), request)
-            // actual address
-            mstore(add(input, 0xa0), hex"0066")
-            // no params
-            // staticcall(gasLimit, to, inputOffset, inputSize, outputOffset, outputSize)
-            if iszero(staticcall(100000000, 0x0e, input, 0x0100, raw_response, 0x0100)) {
-                revert(0, 0)
-            }
-        }
+        bytes memory result = Misc.getDataFromActorResponse(raw_response);
 
         return;
     }
 
     function remove_expired_allocations(
         VerifRegTypes.RemoveExpiredAllocationsParams memory params
-    ) public view returns (VerifRegTypes.RemoveExpiredAllocationsReturn memory) {
+    ) public returns (VerifRegTypes.RemoveExpiredAllocationsReturn memory) {
         bytes memory raw_request = params.serialize();
 
-        // FIXME: find the method num
-        uint64 method_num = 0x00;
+        bytes memory raw_response = Misc.call_actor(2873373899, hex"0005", raw_request);
 
-        // FIXME: unknown size for the response
-        bytes memory raw_response = new bytes(0x0100);
-
-        assembly {
-            let request := mload(add(raw_request, 0x20))
-            let input := mload(0x40)
-            mstore(input, method_num)
-            mstore(add(input, 0x20), CODEC)
-            // address size
-            mstore(add(input, 0x40), 0x02)
-            // params size
-            mstore(add(input, 0x60), mload(raw_request))
-            // actual params
-            mstore(add(input, 0x80), request)
-            // actual address
-            mstore(add(input, 0xa0), hex"0066")
-            // no params
-            // staticcall(gasLimit, to, inputOffset, inputSize, outputOffset, outputSize)
-            if iszero(staticcall(100000000, 0x0e, input, 0x0100, raw_response, 0x0100)) {
-                revert(0, 0)
-            }
-        }
+        bytes memory result = Misc.getDataFromActorResponse(raw_response);
 
         VerifRegTypes.RemoveExpiredAllocationsReturn memory response;
-        response.deserialize(raw_response);
+        response.deserialize(result);
 
         return response;
     }
 
-    function extend_claim_terms(VerifRegTypes.ExtendClaimTermsParams memory params) public view returns (CommonTypes.BatchReturn memory) {
+    function extend_claim_terms(VerifRegTypes.ExtendClaimTermsParams memory params) public returns (CommonTypes.BatchReturn memory) {
         bytes memory raw_request = params.serialize();
 
-        // FIXME: find the method num
-        uint64 method_num = 0x00;
+        bytes memory raw_response = Misc.call_actor(1752273514, hex"0005", raw_request);
 
-        // FIXME: unknown size for the response
-        bytes memory raw_response = new bytes(0x0100);
-
-        assembly {
-            let request := mload(add(raw_request, 0x20))
-            let input := mload(0x40)
-            mstore(input, method_num)
-            mstore(add(input, 0x20), CODEC)
-            // address size
-            mstore(add(input, 0x40), 0x02)
-            // params size
-            mstore(add(input, 0x60), mload(raw_request))
-            // actual params
-            mstore(add(input, 0x80), request)
-            // actual address
-            mstore(add(input, 0xa0), hex"0066")
-            // no params
-            // staticcall(gasLimit, to, inputOffset, inputSize, outputOffset, outputSize)
-            if iszero(staticcall(100000000, 0x0e, input, 0x0100, raw_response, 0x0100)) {
-                revert(0, 0)
-            }
-        }
+        bytes memory result = Misc.getDataFromActorResponse(raw_response);
 
         CommonTypes.BatchReturn memory response;
-        response.deserialize(raw_response);
+        response.deserialize(result);
 
         return response;
     }
 
     function remove_expired_claims(
         VerifRegTypes.RemoveExpiredClaimsParams memory params
-    ) public view returns (VerifRegTypes.RemoveExpiredClaimsReturn memory) {
+    ) public returns (VerifRegTypes.RemoveExpiredClaimsReturn memory) {
         bytes memory raw_request = params.serialize();
 
-        // FIXME: find the method num
-        uint64 method_num = 0x00;
+        bytes memory raw_response = Misc.call_actor(2873373899, hex"0005", raw_request);
 
-        // FIXME: unknown size for the response
-        bytes memory raw_response = new bytes(0x0100);
-
-        assembly {
-            let request := mload(add(raw_request, 0x20))
-            let input := mload(0x40)
-            mstore(input, method_num)
-            mstore(add(input, 0x20), CODEC)
-            // address size
-            mstore(add(input, 0x40), 0x02)
-            // params size
-            mstore(add(input, 0x60), mload(raw_request))
-            // actual params
-            mstore(add(input, 0x80), request)
-            // actual address
-            mstore(add(input, 0xa0), hex"0066")
-            // no params
-            // staticcall(gasLimit, to, inputOffset, inputSize, outputOffset, outputSize)
-            if iszero(staticcall(100000000, 0x0e, input, 0x0100, raw_response, 0x0100)) {
-                revert(0, 0)
-            }
-        }
+        bytes memory result = Misc.getDataFromActorResponse(raw_response);
 
         VerifRegTypes.RemoveExpiredClaimsReturn memory response;
-        response.deserialize(raw_response);
+        response.deserialize(result);
 
         return response;
     }
 
     function universal_receiver_hook(
         VerifRegTypes.UniversalReceiverParams memory params
-    ) public view returns (VerifRegTypes.AllocationsResponse memory) {
+    ) public returns (VerifRegTypes.AllocationsResponse memory) {
         bytes memory raw_request = params.serialize();
 
-        // FIXME: find the method num
-        uint64 method_num = 0x00;
+        bytes memory raw_response = Misc.call_actor(3726118371, hex"0005", raw_request);
 
-        // FIXME: unknown size for the response
-        bytes memory raw_response = new bytes(0x0100);
-
-        assembly {
-            let request := mload(add(raw_request, 0x20))
-            let input := mload(0x40)
-            mstore(input, method_num)
-            mstore(add(input, 0x20), CODEC)
-            // address size
-            mstore(add(input, 0x40), 0x02)
-            // params size
-            mstore(add(input, 0x60), mload(raw_request))
-            // actual params
-            mstore(add(input, 0x80), request)
-            // actual address
-            mstore(add(input, 0xa0), hex"0066")
-            // no params
-            // staticcall(gasLimit, to, inputOffset, inputSize, outputOffset, outputSize)
-            if iszero(staticcall(100000000, 0x0e, input, 0x0100, raw_response, 0x0100)) {
-                revert(0, 0)
-            }
-        }
+        bytes memory result = Misc.getDataFromActorResponse(raw_response);
 
         VerifRegTypes.AllocationsResponse memory response;
-        response.deserialize(raw_response);
+        response.deserialize(result);
 
         return response;
     }
