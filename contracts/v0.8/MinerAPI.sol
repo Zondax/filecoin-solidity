bdd8f53800511e3415af26d2e318e8fc14477e75
Emmanuel <emmanuelm41@gmail.com>
2022-12-02 10:35:23 -0300

    rename target arg name and add docs for it

--- contracts/v0.8/MinerAPI.sol
+++ contracts/v0.8/MinerAPI.sol
@@ -44,11 +44,12 @@ contract MinerAPI {
 
     /// @notice Income and returned collateral are paid to this address
     /// @notice This address is also allowed to change the worker address for the miner
+    /// @param target The miner address (type ID) you want to interact with
     /// @return the owner address of a Miner
-    function get_owner(bytes memory actor_code) public returns (MinerTypes.GetOwnerReturn memory) {
+    function get_owner(bytes memory target) public returns (MinerTypes.GetOwnerReturn memory) {
         bytes memory raw_request = new bytes(0);
 
-        bytes memory raw_response = Actor.call(MinerTypes.GetOwnerMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.GetOwnerMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -58,28 +59,30 @@ contract MinerAPI {
         return response;
     }
 
+    /// @param target The miner address (type ID) you want to interact with
     /// @param addr New owner address
     /// @notice Proposes or confirms a change of owner address.
     /// @notice If invoked by the current owner, proposes a new owner address for confirmation. If the proposed address is the current owner address, revokes any existing proposal that proposed address.
-    function change_owner_address(bytes memory actor_code, bytes memory addr) public {
+    function change_owner_address(bytes memory target, bytes memory addr) public {
         bytes memory raw_request = addr.serializeAddress();
 
-        bytes memory raw_response = Actor.call(MinerTypes.ChangeOwnerAddressMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.ChangeOwnerAddressMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
         return;
     }
 
+    /// @param target The miner address (type ID) you want to interact with
     /// @param addr The "controlling" addresses are the Owner, the Worker, and all Control Addresses.
     /// @return Whether the provided address is "controlling".
     function is_controlling_address(
-        bytes memory actor_code,
+        bytes memory target,
         bytes memory addr
     ) public returns (MinerTypes.IsControllingAddressReturn memory) {
         bytes memory raw_request = addr.serializeAddress();
 
-        bytes memory raw_response = Actor.call(MinerTypes.IsControllingAddressMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.IsControllingAddressMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -90,11 +93,12 @@ contract MinerAPI {
     }
 
     /// @return the miner's sector size.
+    /// @param target The miner address (type ID) you want to interact with
     /// @dev For more information about sector sizes, please refer to https://spec.filecoin.io/systems/filecoin_mining/sector/#section-systems.filecoin_mining.sector
-    function get_sector_size(bytes memory actor_code) public returns (MinerTypes.GetSectorSizeReturn memory) {
+    function get_sector_size(bytes memory target) public returns (MinerTypes.GetSectorSizeReturn memory) {
         bytes memory raw_request = new bytes(0);
 
-        bytes memory raw_response = Actor.call(MinerTypes.GetSectorSizeMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.GetSectorSizeMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -104,13 +108,14 @@ contract MinerAPI {
         return response;
     }
 
+    /// @param target The miner address (type ID) you want to interact with
     /// @notice This is calculated as actor balance - (vesting funds + pre-commit deposit + initial pledge requirement + fee debt)
     /// @notice Can go negative if the miner is in IP debt.
     /// @return the available balance of this miner.
-    function get_available_balance(bytes memory actor_code) public returns (MinerTypes.GetAvailableBalanceReturn memory) {
+    function get_available_balance(bytes memory target) public returns (MinerTypes.GetAvailableBalanceReturn memory) {
         bytes memory raw_request = new bytes(0);
 
-        bytes memory raw_response = Actor.call(MinerTypes.GetAvailableBalanceMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.GetAvailableBalanceMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -120,11 +125,12 @@ contract MinerAPI {
         return response;
     }
 
+    /// @param target The miner address (type ID) you want to interact with
     /// @return the funds vesting in this miner as a list of (vesting_epoch, vesting_amount) tuples.
-    function get_vesting_funds(bytes memory actor_code) public returns (MinerTypes.GetVestingFundsReturn memory) {
+    function get_vesting_funds(bytes memory target) public returns (MinerTypes.GetVestingFundsReturn memory) {
         bytes memory raw_request = new bytes(0);
 
-        bytes memory raw_response = Actor.call(MinerTypes.GetVestingFundsMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.GetVestingFundsMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -134,25 +140,27 @@ contract MinerAPI {
         return response;
     }
 
+    /// @param target The miner address (type ID) you want to interact with
     /// @notice Proposes or confirms a change of beneficiary address.
     /// @notice A proposal must be submitted by the owner, and takes effect after approval of both the proposed beneficiary and current beneficiary, if applicable, any current beneficiary that has time and quota remaining.
     /// @notice See FIP-0029, https://github.com/filecoin-project/FIPs/blob/master/FIPS/fip-0029.md
-    function change_beneficiary(bytes memory actor_code, MinerTypes.ChangeBeneficiaryParams memory params) public {
+    function change_beneficiary(bytes memory target, MinerTypes.ChangeBeneficiaryParams memory params) public {
         bytes memory raw_request = params.serialize();
 
-        bytes memory raw_response = Actor.call(MinerTypes.ChangeBeneficiaryMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.ChangeBeneficiaryMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
         return;
     }
 
+    /// @param target The miner address (type ID) you want to interact with
     /// @notice This method is for use by other actors (such as those acting as beneficiaries), and to abstract the state representation for clients.
     /// @notice Retrieves the currently active and proposed beneficiary information.
-    function get_beneficiary(bytes memory actor_code) public returns (MinerTypes.GetBeneficiaryReturn memory) {
+    function get_beneficiary(bytes memory target) public returns (MinerTypes.GetBeneficiaryReturn memory) {
         bytes memory raw_request = new bytes(0);
 
-        bytes memory raw_response = Actor.call(MinerTypes.GetBeneficiaryMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.GetBeneficiaryMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -163,10 +171,11 @@ contract MinerAPI {
     }
 
     /// @notice FIXME
-    function change_worker_address(bytes memory actor_code, MinerTypes.ChangeWorkerAddressParams memory params) public {
+    /// @param target The miner address (type ID) you want to interact with
+    function change_worker_address(bytes memory target, MinerTypes.ChangeWorkerAddressParams memory params) public {
         bytes memory raw_request = params.serialize();
 
-        bytes memory raw_response = Actor.call(MinerTypes.ChangeWorkerAddressMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.ChangeWorkerAddressMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -174,10 +183,11 @@ contract MinerAPI {
     }
 
     /// @notice FIXME
-    function change_peer_id(bytes memory actor_code, MinerTypes.ChangePeerIDParams memory params) public {
+    /// @param target The miner address (type ID) you want to interact with
+    function change_peer_id(bytes memory target, MinerTypes.ChangePeerIDParams memory params) public {
         bytes memory raw_request = params.serialize();
 
-        bytes memory raw_response = Actor.call(MinerTypes.ChangePeerIDMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.ChangePeerIDMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -185,10 +195,11 @@ contract MinerAPI {
     }
 
     /// @notice FIXME
-    function change_multiaddresses(bytes memory actor_code, MinerTypes.ChangeMultiaddrsParams memory params) public {
+    /// @param target The miner address (type ID) you want to interact with
+    function change_multiaddresses(bytes memory target, MinerTypes.ChangeMultiaddrsParams memory params) public {
         bytes memory raw_request = params.serialize();
 
-        bytes memory raw_response = Actor.call(MinerTypes.ChangeMultiaddrsMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.ChangeMultiaddrsMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -196,10 +207,11 @@ contract MinerAPI {
     }
 
     /// @notice FIXME
-    function repay_debt(bytes memory actor_code) public {
+    /// @param target The miner address (type ID) you want to interact with
+    function repay_debt(bytes memory target) public {
         bytes memory raw_request = new bytes(0);
 
-        bytes memory raw_response = Actor.call(MinerTypes.RepayDebtMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.RepayDebtMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -207,10 +219,11 @@ contract MinerAPI {
     }
 
     /// @notice FIXME
-    function confirm_change_worker_address(bytes memory actor_code) public {
+    /// @param target The miner address (type ID) you want to interact with
+    function confirm_change_worker_address(bytes memory target) public {
         bytes memory raw_request = new bytes(0);
 
-        bytes memory raw_response = Actor.call(MinerTypes.ConfirmChangeWorkerAddressMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.ConfirmChangeWorkerAddressMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -218,10 +231,11 @@ contract MinerAPI {
     }
 
     /// @notice FIXME
-    function get_peer_id(bytes memory actor_code) public returns (MinerTypes.GetPeerIDReturn memory) {
+    /// @param target The miner address (type ID) you want to interact with
+    function get_peer_id(bytes memory target) public returns (MinerTypes.GetPeerIDReturn memory) {
         bytes memory raw_request = new bytes(0);
 
-        bytes memory raw_response = Actor.call(MinerTypes.GetPeerIDMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.GetPeerIDMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
@@ -232,10 +246,11 @@ contract MinerAPI {
     }
 
     /// @notice FIXME
-    function get_multiaddresses(bytes memory actor_code) public returns (MinerTypes.GetMultiaddrsReturn memory) {
+    /// @param target The miner address (type ID) you want to interact with
+    function get_multiaddresses(bytes memory target) public returns (MinerTypes.GetMultiaddrsReturn memory) {
         bytes memory raw_request = new bytes(0);
 
-        bytes memory raw_response = Actor.call(MinerTypes.GetMultiaddrsMethodNum, actor_code, raw_request);
+        bytes memory raw_response = Actor.call(MinerTypes.GetMultiaddrsMethodNum, target, raw_request);
 
         bytes memory result = Actor.readRespData(raw_response);
 
