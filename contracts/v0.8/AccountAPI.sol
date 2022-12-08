4859d34fdd7847858be5d12c82ff2bbe61696f30
Emmanuel <emmanuelm41@gmail.com>
2022-12-08 09:04:52 -0300

    add base code for the actor

---
+++ contracts/v0.8/AccountAPI.sol
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ *   (c) 2022 Zondax AG
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ********************************************************************************/
+//
+// DRAFT!! THIS CODE HAS NOT BEEN AUDITED - USE ONLY FOR PROTOTYPING
+//
+pragma solidity >=0.4.25 <=0.8.15;
+
+import "./types/AccountTypes.sol";
+import "./cbor/AccountCbor.sol";
+import "./types/CommonTypes.sol";
+import "./utils/Misc.sol";
+import "./utils/Actor.sol";
+
+/// @title This contract is a proxy to the singleton Account actor (address: f0X). Calling one of its methods will result in a cross-actor call being performed.
+/// @author Zondax AG
+contract AccountAPI {
+    using AuthenticateMessageCBOR for AccountTypes.AuthenticateMessageParams;
+    using BytesCBOR for bytes;
+
+    /// @notice FIXME
+    function authenticate_message(AccountTypes.AuthenticateMessageParams memory params) public {
+        bytes memory raw_request = params.serialize();
+
+        bytes memory raw_response = Actor.call(AccountTypes.AuthenticateMessageMethodNum, AccountTypes.ActorCode, raw_request);
+
+        bytes memory result = Actor.readRespData(raw_response);
+    }
+
+    /// @notice FIXME
+    function universal_receiver_hook(bytes memory params) public {
+        bytes memory raw_request = params.serializeBytes();
+
+        bytes memory raw_response = Actor.call(AccountTypes.UniversalReceiverHookMethodNum, AccountTypes.ActorCode, raw_request);
+
+        bytes memory result = Actor.readRespData(raw_response);
+    }
+}
---
+++ contracts/v0.8/types/AccountTypes.sol
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ *   (c) 2022 Zondax AG
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ********************************************************************************/
+//
+// DRAFT!! THIS CODE HAS NOT BEEN AUDITED - USE ONLY FOR PROTOTYPING
+//
+pragma solidity >=0.4.25 <=0.8.15;
+
+import "./CommonTypes.sol";
+
+/// @title Filecoin account actor types for Solidity.
+/// @author Zondax AG
+library AccountTypes {
+    bytes constant ActorCode = hex"0005"; // FIXME set this value correctly
+    uint constant AuthenticateMessageMethodNum = 2643134072;
+    uint constant UniversalReceiverHookMethodNum = 3726118371;
+
+    struct AuthenticateMessageParams {
+        bytes signature;
+        bytes message;
+    }
+}
