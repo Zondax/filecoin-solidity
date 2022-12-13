812b08154672f25bf4e6201a5a2e5b5b0988f8d9
Emmanuel <emmanuelm41@gmail.com>
2022-12-13 10:20:43 -0300

    create a new multisig actor in order to test api (#76)
    * create a new multisig actor in order to test api
    * fix ci descriptions
    * call propose method (wip)
    * add some new tests

--- testing/multisig/src/main.rs
+++ testing/multisig/src/main.rs
@@ -1,16 +1,26 @@
 use fvm_integration_tests::tester::{Account, Tester};
 use fvm_integration_tests::dummy::DummyExterns;
 use fvm_integration_tests::bundle;
-use fvm_ipld_encoding::{strict_bytes, tuple::*};
+use fvm_ipld_encoding::{strict_bytes, BytesDe, tuple::*};
 use fvm_shared::state::StateTreeVersion;
 use fvm_shared::version::NetworkVersion;
 use fvm_ipld_blockstore::MemoryBlockstore;
+use fvm_shared::address::Address;
 use std::env;
 use fvm_shared::message::Message;
 use fvm::executor::{ApplyKind, Executor};
 use fil_actor_eam::Return;
+use fil_actor_init::ExecReturn;
 use fvm_ipld_encoding::RawBytes;
-use fil_actors_runtime::{EAM_ACTOR_ADDR};
+use fil_actors_runtime::{EAM_ACTOR_ADDR, INIT_ACTOR_ADDR};
+use cid::Cid;
+use std::str::FromStr;
+use rand_core::OsRng;
+use bls_signatures::Serialize;
+use multihash::Code;
+use fvm_ipld_encoding::CborStore;
+use fvm::state_tree::{ActorState};
+use fvm_shared::econ::TokenAmount;
 
 const WASM_COMPILED_PATH: &str =
    "../../build/v0.8/MultisigAPI.bin";
@@ -35,10 +45,54 @@ fn main() {
 
     let sender: [Account; 1] = tester.create_accounts().unwrap();
 
+    println!("Sender address id");
+    dbg!(&sender[0].0);
+    println!("Sender address bytes");
+    dbg!(hex::encode(&sender[0].1.to_bytes()));
+    /***********************************************
+     *
+     * Instantiate Account Actor with a BLS address
+     *
+     ***********************************************/
+
+    let bls_private_key = bls_signatures::PrivateKey::generate(&mut OsRng);
+    let worker = Address::new_bls(&bls_private_key.public_key().as_bytes()).unwrap();
+
+    let state_tree = tester
+        .state_tree
+        .as_mut()
+        .unwrap();
+    let assigned_addr = state_tree.register_new_address(&worker).unwrap();
+    let state = fvm::account_actor::State {
+        address: worker,
+    };
+
+    let cid = state_tree.store().put_cbor(&state, Code::Blake2b256).unwrap();
+
+    let actor_state = ActorState {
+        // CID of Accounts actor. You get this as output from builtin-actors compiling process
+        code: Cid::from_str("bafk2bzacebu625ptoi4ssmpr3nndjs3ujybdcdoiozjx64jvnzugypo7sm7os").unwrap(),
+        // code: Cid::from_str("bafk2bzacecj7v5ur5qk4vn3xbvgsizl35e42l3yaankmxu6dcoouv4mkphsjq").unwrap(),
+        state: cid,
+        sequence: 0,
+        balance: TokenAmount::from_atto(10000),
+        address: Some(worker),
+    };
+
+    state_tree
+        .set_actor(assigned_addr, actor_state)
+        .unwrap();
     // Instantiate machine
     tester.instantiate_machine(DummyExterns).unwrap();
 
     let executor = tester.executor.as_mut().unwrap();
+
+    /**************************
+     *
+     * Machine instantiated
+     *
+     **************************/
+
 
     println!("Calling init actor (EVM)");
 
@@ -60,6 +114,7 @@ fn main() {
         to: EAM_ACTOR_ADDR,
         gas_limit: 1000000000,
         method_num: 3,
+        sequence: 0,
         params: RawBytes::serialize(constructor_params).unwrap(),
         ..Message::default()
     };
@@ -70,6 +125,99 @@ fn main() {
 
     assert_eq!(res.msg_receipt.exit_code.value(), 0);
 
-    let _exec_return : Return = RawBytes::deserialize(&res.msg_receipt.return_data).unwrap();
+    let exec_return : Return = RawBytes::deserialize(&res.msg_receipt.return_data).unwrap();
 
+    println!("Contract actor id");
+    dbg!(&exec_return.actor_id);
+    let contract_actor_id = exec_return.actor_id;
+
+    println!("Create Multisig actor for solidity contract to interact with");
+
+    let constructor_params = fil_actor_multisig::ConstructorParams{
+        num_approvals_threshold: 1,
+        signers: vec![Address::new_id(sender[0].0),Address::new_id(contract_actor_id)],
+        start_epoch: 1670873233,
+        unlock_duration: 1670873234
+    };
+
+    let exec_params = fil_actor_init::ExecParams{
+        // CID of StorageMiner actor. You get this as output from builtin-actors compiling process
+        code_cid: Cid::from_str("bafk2bzacebjppmys5tqoq2fquf6vkb2bfcgsncfwy7bms7zgt37gowvphjsfk").unwrap(),
+        // code_cid: Cid::from_str("bafk2bzacedgixfd465634uihet3u57vugbbp6s5sseb76phti3cexx66ers3i").unwrap(),
+        constructor_params: RawBytes::serialize(constructor_params).unwrap(),
+    };
+
+    let message = Message {
+        from: sender[0].1,
+        to: INIT_ACTOR_ADDR,
+        gas_limit: 1000000000,
+        method_num: 2,
+        sequence: 1,
+        params: RawBytes::serialize(exec_params).unwrap(),
+        ..Message::default()
+    };
+
+    let res = executor
+        .execute_message(message, ApplyKind::Explicit, 100)
+        .unwrap();
+
+    let exec_return : ExecReturn = RawBytes::deserialize(&res.msg_receipt.return_data).unwrap();
+
+    println!("Multisig actor address bytes");
+    dbg!(hex::encode(&exec_return.id_address.to_bytes()));
+
+    assert_eq!(res.msg_receipt.exit_code.value(), 0);
+
+
+    println!("Calling `propose`");
+
+    let message = Message {
+        from: sender[0].1,
+        to: Address::new_id(contract_actor_id),
+        gas_limit: 1000000000,
+        method_num: 2,
+        sequence: 2,
+        params: RawBytes::new(hex::decode("590164C0987260000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000002006600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000457000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000C0000000000000000000000000000000000000000000000000000000000000000200640000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap()),
+        ..Message::default()
+    };
+
+    let res = executor
+        .execute_message(message, ApplyKind::Explicit, 100)
+        .unwrap();
+
+    if res.msg_receipt.exit_code.value() != 33 {
+        dbg!(&res);
+    }
+
+    assert_eq!(res.msg_receipt.exit_code.value(), 33);
+    assert_eq!(hex::encode(res.msg_receipt.return_data.bytes()), "08c379a0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000136163746f72206572726f7220636f646520333300000000000000000000000000");
+
+
+
+    println!("Calling `add_signer`");
+
+    let message = Message {
+        from: sender[0].1,
+        to: Address::new_id(contract_actor_id),
+        gas_limit: 1000000000,
+        method_num: 2,
+        sequence: 3,
+        params: RawBytes::new(hex::decode("5901041339400D00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000200670000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000020064000000000000000000000000000000000000000000000000000000000000").unwrap()),
+        ..Message::default()
+    };
+
+    let res = executor
+        .execute_message(message, ApplyKind::Explicit, 100)
+        .unwrap();
+
+    if res.msg_receipt.exit_code.value() != 33 {
+        dbg!(&res);
+    }
+
+    assert_eq!(res.msg_receipt.exit_code.value(), 33);
+    assert_eq!(hex::encode(res.msg_receipt.return_data.bytes()), "08c379a0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000136163746f72206572726f7220636f646520313800000000000000000000000000");
+
+
+
+    // FIXME: As propose is failing, we cannot execute the rest of the methods...
 }
