c911197ec27cca0693c934550af05ac0c8d73dbf
rllola <rllola80@gmail.com>
2022-12-13 15:37:50 +0100

    use new precompile format

--- contracts/v0.8/utils/Actor.sol
+++ contracts/v0.8/utils/Actor.sol
@@ -25,6 +25,7 @@ library Actor {
     uint64 constant GAS_LIMIT = 100000000;
     uint64 constant CALL_ACTOR_PRECOMPILE_ADDR = 0x0e;
     uint64 constant MAX_RAW_RESPONSE_SIZE = 0x300;
+    uint64 constant READ_FLAG = 0x00000001; // https://github.com/filecoin-project/ref-fvm/blob/master/shared/src/sys/mod.rs#L60
 
     function call(uint method_num, bytes memory actor_code, bytes memory raw_request) internal returns (bytes memory) {
         bytes memory raw_response = new bytes(MAX_RAW_RESPONSE_SIZE);
@@ -38,11 +39,16 @@ library Actor {
             len := mload(raw_request)
             let input := mload(0x40)
             mstore(input, method_num)
-            mstore(add(input, 0x20), CODEC)
+            // value to send
+            mstore(add(input, 0x20), amount)
+            // readonly flag is mandatory for now
+            mstore(add(input, 0x40), READ_FLAG)
+            // cbor codec is mandatory for now
+            mstore(add(input, 0x60), CODEC)
             // address size
-            mstore(add(input, 0x40), mload(actor_code))
+            mstore(add(input, 0x80), mload(actor_code))
             // params size
-            mstore(add(input, 0x60), len)
+            mstore(add(input, 0xa0), len)
             // actual params (copy by slice of 32 bytes)
             let offset := 0
             for {
@@ -50,18 +56,18 @@ library Actor {
             } lt(offset, len) {
                 offset := add(offset, 0x20)
             } {
-                mstore(add(input, add(0x80, offset)), mload(add(raw_request, add(0x20, offset))))
+                mstore(add(input, add(0xc0, offset)), mload(add(raw_request, add(0x20, offset))))
             }
             if mod(len, 0x20) {
                 offset := add(sub(offset, 0x20), mod(len, 0x20))
             }
             // actual address
-            mstore(add(input, add(0x80, offset)), mload(add(actor_code, 0x20)))
+            mstore(add(input, add(0xc0, offset)), mload(add(actor_code, 0x20)))
             // no params
 
             // FIXME set inputSize according to the input length
-            // call(gasLimit, to, amount, inputOffset, inputSize, outputOffset, outputSize)
-            if iszero(call(GAS_LIMIT, CALL_ACTOR_PRECOMPILE_ADDR, amount, input, 0x100, raw_response, MAX_RAW_RESPONSE_SIZE)) {
+            // delegatecall(gasLimit, to, inputOffset, inputSize, outputOffset, outputSize)
+            if iszero(delegatecall(GAS_LIMIT, CALL_ACTOR_PRECOMPILE_ADDR, input, 0x100, raw_response, MAX_RAW_RESPONSE_SIZE)) {
                 revert(0, 0)
             }
         }
