ce28f722107956b3a09a85e945d06ddc6aaa7ddf
Emmanuel <emmanuelm41@gmail.com>
2022-12-16 22:24:02 -0300

    read int256 and uint256 from bytes correctly

--- contracts/v0.8/utils/Misc.sol
+++ contracts/v0.8/utils/Misc.sol
@@ -1,22 +1,30 @@
 /*******************************************************************************
-*   (c) 2022 Zondax AG
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-********************************************************************************/
+ *   (c) 2022 Zondax AG
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ********************************************************************************/
 // DRAFT!! THIS CODE HAS NOT BEEN AUDITED - USE ONLY FOR PROTOTYPING
 pragma solidity >=0.4.25 <=0.8.15;
 
 library Misc {
+    function toUint256(bytes32 _bytes) internal pure returns (uint256 value) {
+        return uint256(_bytes);
+    }
+
+    function toInt256(bytes32 _bytes) internal pure returns (int256 value) {
+        return int256(uint256(_bytes));
+    }
+
     function toUint256(bytes memory _bytes, uint offset) internal pure returns (uint256 value) {
         assembly {
             value := mload(add(_bytes, offset))
