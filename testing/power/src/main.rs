e36221543bf187c545f488b053dbf91b50866ce8
sleepy ramen <rllola80@gmail.com>
2022-12-26 14:13:29 +0100

    fix test ppower (#104)

--- testing/power/src/main.rs
+++ testing/power/src/main.rs
@@ -12,6 +12,16 @@ use fvm::executor::{ApplyKind, Executor};
 use fil_actor_eam::Return;
 use fvm_ipld_encoding::RawBytes;
 use fil_actors_runtime::{EAM_ACTOR_ADDR};
+use bls_signatures::Serialize;
+use fvm_shared::sector::RegisteredPoStProof;
+use fvm_ipld_encoding::BytesDe;
+use multihash::Code;
+use rand_core::OsRng;
+use fvm_shared::econ::TokenAmount;
+use fvm_ipld_encoding::CborStore;
+use fvm::state_tree::{ActorState};
+use cid::Cid;
+use std::str::FromStr;
 
 const WASM_COMPILED_PATH: &str =
    "../../build/v0.8/PowerAPI.bin";
@@ -22,6 +32,16 @@ pub struct Create2Params {
     pub initcode: Vec<u8>,
     #[serde(with = "strict_bytes")]
     pub salt: [u8; 32],
+}
+
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
+pub struct CreateMinerParams {
+    pub owner: Address,
+    pub worker: Address,
+    pub window_post_proof_type: RegisteredPoStProof,
+    #[serde(with = "strict_bytes")]
+    pub peer: Vec<u8>,
+    pub multiaddrs: Vec<BytesDe>,
 }
 
 fn main() {
@@ -36,10 +56,69 @@ fn main() {
 
     let sender: [Account; 1] = tester.create_accounts().unwrap();
 
+    /***********************************************
+     *
+     * Instantiate Account Actor with a BLS address
+     *
+     ***********************************************/
+
+    let bls_private_key_provider = bls_signatures::PrivateKey::generate(&mut OsRng);
+    let worker = Address::new_bls(&bls_private_key_provider.public_key().as_bytes()).unwrap();
+
+    let state_tree = tester
+        .state_tree
+        .as_mut()
+        .unwrap();
+    let assigned_addr = state_tree.register_new_address(&worker).unwrap();
+    let state = fvm::account_actor::State {
+        address: worker,
+    };
+
+    let cid = state_tree.store().put_cbor(&state, Code::Blake2b256).unwrap();
+
+    let actor_state = ActorState {
+        // CID of Accounts actor. You get this as output from builtin-actors compiling process
+        code: Cid::from_str("bafk2bzacecgc56qrvqorqcgxeyoc6cwccl2zwtsewjq7c4j7le2avixe6hm4a").unwrap(),
+        //code: Cid::from_str("bafk2bzaceddmas33nnn2izdexi5xjzuahzezl62aa5ah5bqwzzjceusskr6ty").unwrap(),
+        state: cid,
+        sequence: 0,
+        balance: TokenAmount::from_whole(1_000_000),
+        address: Some(worker),
+    };
+
+    state_tree
+        .set_actor(assigned_addr, actor_state)
+        .unwrap();
+
     // Instantiate machine
     tester.instantiate_machine(DummyExterns).unwrap();
 
     let executor = tester.executor.as_mut().unwrap();
+
+    println!("Create Miner actor to be able to claim power");
+
+    let constructor_params = CreateMinerParams{
+        owner: sender[0].1,
+        worker,
+        window_post_proof_type: fvm_shared::sector::RegisteredPoStProof::StackedDRGWindow2KiBV1,
+        peer: vec![1, 2, 3],
+        multiaddrs: vec![BytesDe(vec![1, 2, 3])],
+    };
+
+    let message = Message {
+        from: sender[0].1,
+        to: Address::new_id(4),
+        gas_limit: 1000000000,
+        method_num: 2,
+        params: RawBytes::serialize(constructor_params).unwrap(),
+        ..Message::default()
+    };
+
+    let res = executor
+    .execute_message(message, ApplyKind::Explicit, 100)
+    .unwrap();
+
+    assert_eq!(res.msg_receipt.exit_code.value(), 0);
 
     println!("Calling init actor (EVM)");
 
@@ -61,6 +140,7 @@ fn main() {
         to: EAM_ACTOR_ADDR,
         gas_limit: 1000000000,
         method_num: 3,
+        sequence: 1,
         params: RawBytes::serialize(constructor_params).unwrap(),
         ..Message::default()
     };
@@ -80,7 +160,7 @@ fn main() {
         to: Address::new_id(exec_return.actor_id),
         gas_limit: 1000000000,
         method_num: 2,
-        sequence: 1,
+        sequence: 2,
         params: RawBytes::new(hex::decode("4487A8D7D6").unwrap()),
         ..Message::default()
     };
@@ -98,7 +178,7 @@ fn main() {
         to: Address::new_id(exec_return.actor_id),
         gas_limit: 1000000000,
         method_num: 2,
-        sequence: 2,
+        sequence: 3,
         params: RawBytes::new(hex::decode("446C3D7356").unwrap()),
         ..Message::default()
     };
@@ -116,8 +196,8 @@ fn main() {
         to: Address::new_id(exec_return.actor_id),
         gas_limit: 1000000000,
         method_num: 2,
-        sequence: 3,
-        params: RawBytes::new(hex::decode("5824E7530A9B000000000000000000000000000000000000000000000000000000000000003D").unwrap()),
+        sequence: 4,
+        params: RawBytes::new(hex::decode("5824E7530A9B0000000000000000000000000000000000000000000000000000000000000066").unwrap()),
         ..Message::default()
     };
 
@@ -125,8 +205,7 @@ fn main() {
         .execute_message(message, ApplyKind::Explicit, 100)
         .unwrap();
 
-    // FIXME
-    assert_eq!(res.msg_receipt.exit_code.value(), 33);
+    assert_eq!(res.msg_receipt.exit_code.value(), 0);
 
     println!("Calling `miner_consensus_count`");
 
@@ -135,7 +214,7 @@ fn main() {
         to: Address::new_id(exec_return.actor_id),
         gas_limit: 1000000000,
         method_num: 2,
-        sequence: 4,
+        sequence: 5,
         params: RawBytes::new(hex::decode("44DF16B842").unwrap()),
         ..Message::default()
     };
