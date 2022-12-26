b2f7a90dfaba1bb9a8cffe1ee605275be3c98b30
Natanael <neithanmo@gmail.com>
2022-12-26 13:42:18 +0100

    test: VerifiedReg actor and test for AddVerifiedClient and GetClaims

--- testing/verifreg/src/main.rs
+++ testing/verifreg/src/main.rs
@@ -1,21 +1,23 @@
-use fvm_integration_tests::tester::{Account, Tester};
-use fvm_integration_tests::dummy::DummyExterns;
-use fvm_integration_tests::bundle;
-use fvm_ipld_encoding::{strict_bytes, tuple::*};
-use fvm_shared::state::StateTreeVersion;
-use fvm_shared::version::NetworkVersion;
-use fvm_ipld_blockstore::MemoryBlockstore;
-use std::env;
-use fvm_shared::message::Message;
-use fvm::executor::{ApplyKind, Executor};
 use fil_actor_eam::Return;
+use fil_actors_runtime::EAM_ACTOR_ADDR;
+
+use fvm::executor::{ApplyKind, Executor};
+use fvm_integration_tests::bundle;
+use fvm_integration_tests::dummy::DummyExterns;
+use fvm_integration_tests::tester::{Account, Tester};
+use fvm_ipld_blockstore::MemoryBlockstore;
 use fvm_ipld_encoding::RawBytes;
-use fil_actors_runtime::{EAM_ACTOR_ADDR};
+use fvm_ipld_encoding::{strict_bytes, tuple::*};
 use fvm_shared::address::Address;
 
+use fvm_shared::message::Message;
+use fvm_shared::state::StateTreeVersion;
+use fvm_shared::version::NetworkVersion;
+use std::env;
 
-const WASM_COMPILED_PATH: &str =
-   "../../build/v0.8/VerifRegAPI.bin";
+const WASM_COMPILED_PATH: &str = "../../build/v0.8/VerifRegAPI.bin";
+
+pub const ROOT_ADDR: Address = Address::new_id(101);
 
 #[derive(Serialize_tuple, Deserialize_tuple)]
 pub struct Create2Params {
@@ -29,7 +31,8 @@ fn main() {
     println!("Testing solidity API");
 
     let bs = MemoryBlockstore::default();
-    let actors = std::fs::read("../builtin-actors/output/builtin-actors-devnet-wasm.car").expect("Unable to read actor devnet file file");
+    let actors = std::fs::read("../builtin-actors/output/builtin-actors-devnet-wasm.car")
+        .expect("Unable to read actor devnet file");
     let bundle_root = bundle::import_bundle(&bs, &actors).unwrap();
 
     let mut tester =
@@ -48,7 +51,7 @@ fn main() {
         .unwrap()
         .join(WASM_COMPILED_PATH)
         .canonicalize()
-        .unwrap();
+        .expect("Unexpected wasm path");
     let evm_hex = std::fs::read(wasm_path).expect("Unable to read file");
     let evm_bin = hex::decode(evm_hex).unwrap();
 
@@ -62,27 +65,27 @@ fn main() {
         to: EAM_ACTOR_ADDR,
         gas_limit: 1000000000,
         method_num: 3,
+        sequence: 0,
         params: RawBytes::serialize(constructor_params).unwrap(),
         ..Message::default()
     };
 
     let res = executor
         .execute_message(message, ApplyKind::Explicit, 100)
-        .unwrap();
+        .expect("Fail to initialize verifiedReg actor");
 
     assert_eq!(res.msg_receipt.exit_code.value(), 0);
 
-    let exec_return : Return = RawBytes::deserialize(&res.msg_receipt.return_data).unwrap();
+    let exec_return: Return = RawBytes::deserialize(&res.msg_receipt.return_data).unwrap();
 
     println!("Calling `add_verified_client`");
-
     let message = Message {
         from: sender[0].1,
         to: Address::new_id(exec_return.actor_id),
         gas_limit: 1000000000,
         method_num: 2,
         sequence: 1,
-        params: RawBytes::new(hex::decode("58E4557074610000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000002006400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030010000000000000000000000000000000000000000000000000000000000000").unwrap()),
+        params: RawBytes::new(hex::decode("58E4557074610000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000001C90000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000031000000000000000000000000000000000000000000000000000000000000000").unwrap()),
         ..Message::default()
     };
 
@@ -90,6 +93,25 @@ fn main() {
         .execute_message(message, ApplyKind::Explicit, 100)
         .unwrap();
 
-    // FIXME: "caller f0101 is not a verifier"
+    // FIXME: "caller f0201 is not a verifier"
     assert_eq!(res.msg_receipt.exit_code.value(), 33);
+
+    println!("Calling `claim_allocations`");
+
+    let message = Message {
+        from: sender[0].1,
+        to: Address::new_id(exec_return.actor_id),
+        gas_limit: 1000000000,
+        method_num: 2,
+        sequence: 2,
+        params: RawBytes::new(hex::decode("58C4DBCB98AB00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000065000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000A").unwrap()),
+        ..Message::default()
+    };
+
+    let res = executor
+        .execute_message(message, ApplyKind::Explicit, 100)
+        .unwrap();
+
+    // Should not fail as actor would return an empty list of allocations
+    assert_eq!(res.msg_receipt.exit_code.value(), 0);
 }
