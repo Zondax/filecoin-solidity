ced9a5880ab8c31847b9401b3663be36163b325f
Emmanuel <emmanuelm41@gmail.com>
2023-01-04 16:24:00 -0300

    feat: Change int256 for BigNumber struct for VerifReg actor

--- contracts/v0.8/types/VerifRegTypes.sol
+++ contracts/v0.8/types/VerifRegTypes.sol
@@ -1,23 +1,24 @@
 /*******************************************************************************
-*   (c) 2022 Zondax AG
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License.
-********************************************************************************/
+ *   (c) 2022 Zondax AG
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ********************************************************************************/
 //
 // DRAFT!! THIS CODE HAS NOT BEEN AUDITED - USE ONLY FOR PROTOTYPING
 //
 pragma solidity >=0.4.25 <=0.8.17;
 
+import "../cbor/BigNumberCbor.sol";
 import "./CommonTypes.sol";
 
 /// @title Filecoin Verified Registry actor types for Solidity.
@@ -56,7 +57,7 @@ library VerifRegTypes {
         // Results for each processed allocation.
         CommonTypes.BatchReturn results;
         // The amount of datacap reclaimed for the client.
-        int256 datacap_recovered;
+        BigNumber datacap_recovered;
     }
     struct RemoveExpiredClaimsParams {
         // Provider to clean up (need not be the caller)
