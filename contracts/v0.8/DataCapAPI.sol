d4466b08b83257ee52b495444b06a903d4c8fe3d
Emmanuel <emmanuelm41@gmail.com>
2023-01-30 12:33:33 -0300

    Fix solc version to ^0.8.17 (#189)

--- contracts/v0.8/DataCapAPI.sol
+++ contracts/v0.8/DataCapAPI.sol
@@ -17,7 +17,7 @@
 // DRAFT!! THIS CODE HAS NOT BEEN AUDITED - USE ONLY FOR PROTOTYPING
 
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity >=0.4.25 <=0.8.17;
+pragma solidity ^0.8.17;
 
 import "./types/DataCapTypes.sol";
 import "./cbor/DataCapCbor.sol";
