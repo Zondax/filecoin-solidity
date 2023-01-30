195d7eddfa9e44e1c1150884f9d6783067df5360
Emmanuel <emmanuelm41@gmail.com>
2023-01-30 12:49:54 -0300

    fix: use hyperspace on hardhat tests

--- hardhat/tasks/get-address.js
+++ hardhat/tasks/get-address.js
@@ -16,52 +16,50 @@
 //
 // DRAFT!! THIS CODE HAS NOT BEEN AUDITED - USE ONLY FOR PROTOTYPING
 //
-const fa = require("@glif/filecoin-address");
-const util = require("util");
-const request = util.promisify(require("request"));
+const fa = require("@glif/filecoin-address")
+const util = require("util")
+const request = util.promisify(require("request"))
 
-task("get-address", "Gets Filecoin f4 address and corresponding Ethereum address.")
-  .setAction(async (taskArgs) => {
+task("get-address", "Gets Filecoin f4 address and corresponding Ethereum address.").setAction(
+    async (taskArgs) => {
+        const DEPLOYER_PRIVATE_KEY = network.config.accounts[0]
 
-const DEPLOYER_PRIVATE_KEY = network.config.accounts[0]
+        function hexToBytes(hex) {
+            for (var bytes = [], c = 0; c < hex.length; c += 2)
+                bytes.push(parseInt(hex.substr(c, 2), 16))
+            return new Uint8Array(bytes)
+        }
 
-function hexToBytes(hex) {
-  for (var bytes = [], c = 0; c < hex.length; c += 2)
-    bytes.push(parseInt(hex.substr(c, 2), 16));
-  return new Uint8Array(bytes);
-}
+        async function callRpc(method, params) {
+            var options = {
+                method: "POST",
+                url: "https://api.zondax.ch/fil/node/hyperspace/rpc/v0",
+                // url: "http://localhost:1234/rpc/v0",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    jsonrpc: "2.0",
+                    method: method,
+                    params: params,
+                    id: 1,
+                }),
+            }
+            const res = await request(options)
+            return JSON.parse(res.body).result
+        }
 
-async function callRpc(method, params) {
-  var options = {
-    method: "POST",
-    url: "https://wallaby.node.glif.io/rpc/v0",
-    // url: "http://localhost:1234/rpc/v0",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      jsonrpc: "2.0",
-      method: method,
-      params: params,
-      id: 1,
-    }),
-  };
-  const res = await request(options);
-  return JSON.parse(res.body).result;
-}
+        const deployer = new ethers.Wallet(DEPLOYER_PRIVATE_KEY)
 
-const deployer = new ethers.Wallet(DEPLOYER_PRIVATE_KEY);
+        const pubKey = hexToBytes(deployer.publicKey.slice(2))
 
+        const priorityFee = await callRpc("eth_maxPriorityFeePerGas")
 
-  const pubKey = hexToBytes(deployer.publicKey.slice(2));
-
-  const priorityFee = await callRpc("eth_maxPriorityFeePerGas");
-
-  const f4Address = fa.newDelegatedEthAddress(deployer.address).toString();
-  const nonce = await callRpc("Filecoin.MpoolGetNonce", [f4Address]);
-  console.log("f4address (for use with faucet) = ", f4Address);
-  console.log("Ethereum address:", deployer.address);
-})
-
+        const f4Address = fa.newDelegatedEthAddress(deployer.address).toString()
+        const nonce = await callRpc("Filecoin.MpoolGetNonce", [f4Address])
+        console.log("f4address (for use with faucet) = ", f4Address)
+        console.log("Ethereum address:", deployer.address)
+    }
+)
 
 module.exports = {}
--- hardhat/tasks/miner-api/change-beneficiary.js
+++ hardhat/tasks/miner-api/change-beneficiary.js
@@ -16,54 +16,53 @@
 //
 // DRAFT!! THIS CODE HAS NOT BEEN AUDITED - USE ONLY FOR PROTOTYPING
 //
-const util = require("util");
+const util = require("util")
 
-const request = util.promisify(require("request"));
+const request = util.promisify(require("request"))
 
 task("change-beneficiary", "")
-  .addParam("contractaddress", "The SimpleCoin address")
-  .addParam("beneficiary", "")
-  .addParam("quota", "")
-  .addParam("expiration", "")
-  .setAction(async (taskArgs) => {
-  const contractAddr = taskArgs.contractaddress
-    const {beneficiary, quota, expiration} = taskArgs
-    const networkId = network.name
+    .addParam("contractaddress", "The SimpleCoin address")
+    .addParam("beneficiary", "")
+    .addParam("quota", "")
+    .addParam("expiration", "")
+    .setAction(async (taskArgs) => {
+        const contractAddr = taskArgs.contractaddress
+        const { beneficiary, quota, expiration } = taskArgs
+        const networkId = network.name
 
-    const priorityFee = await callRpc("eth_maxPriorityFeePerGas")
+        const priorityFee = await callRpc("eth_maxPriorityFeePerGas")
 
-    async function callRpc(method, params) {
-      var options = {
-        method: "POST",
-        url: "https://wallaby.node.glif.io/rpc/v0",
-        // url: "http://localhost:1234/rpc/v0",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          jsonrpc: "2.0",
-          method: method,
-          params: params,
-          id: 1,
-        }),
-      };
-      const res = await request(options);
-      return JSON.parse(res.body).result;
-    }
+        async function callRpc(method, params) {
+            var options = {
+                method: "POST",
+                url: "https://api.zondax.ch/fil/node/hyperspace/rpc/v0",
+                // url: "http://localhost:1234/rpc/v0",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    jsonrpc: "2.0",
+                    method: method,
+                    params: params,
+                    id: 1,
+                }),
+            }
+            const res = await request(options)
+            return JSON.parse(res.body).result
+        }
 
-    console.log("Calling change_beneficiary method")
-    const MinerAPI = await ethers.getContractFactory("MinerAPI")
+        console.log("Calling change_beneficiary method")
+        const MinerAPI = await ethers.getContractFactory("MinerAPI")
 
-    //Get signer information
-    const accounts = await ethers.getSigners()
-    const signer = accounts[0]
+        //Get signer information
+        const accounts = await ethers.getSigners()
+        const signer = accounts[0]
 
-
-    const minerAPIContract = new ethers.Contract(contractAddr, MinerAPI.interface, signer)
-    await minerAPIContract.change_beneficiary([beneficiary, quota, expiration], {
-      gasLimit: 1000000000,
-      maxPriorityFeePerGas: priorityFee
+        const minerAPIContract = new ethers.Contract(contractAddr, MinerAPI.interface, signer)
+        await minerAPIContract.change_beneficiary([beneficiary, quota, expiration], {
+            gasLimit: 1000000000,
+            maxPriorityFeePerGas: priorityFee,
+        })
     })
-  })
 
 module.exports = {}
