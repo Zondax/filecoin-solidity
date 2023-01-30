5d667dda4116778335beb66f7062c36dc7a87ed4
Emmanuel <emmanuelm41@gmail.com>
2023-01-30 15:18:26 -0300

    feat: refactor cbor functions for account actor and utils library

--- contracts/v0.8/AccountAPI.sol
+++ contracts/v0.8/AccountAPI.sol
@@ -22,21 +22,19 @@ pragma solidity ^0.8.17;
 import "./types/AccountTypes.sol";
 import "./cbor/AccountCbor.sol";
 import "./cbor/BytesCbor.sol";
-import "./types/CommonTypes.sol";
 import "./utils/Misc.sol";
 import "./utils/Actor.sol";
 
 /// @title This contract is a proxy to the Account actor. Calling one of its methods will result in a cross-actor call being performed.
 /// @author Zondax AG
 library AccountAPI {
-    using AuthenticateMessageCBOR for AccountTypes.AuthenticateMessageParams;
-    using UniversalReceiverHookCBOR for AccountTypes.UniversalReceiverParams;
+    using AccountCBOR for *;
     using BytesCBOR for bytes;
 
     /// @notice TODO fill this a proper description
     /// @param target The account address (filecoin bytes format) you want to interact with
     function authenticateMessage(bytes memory target, AccountTypes.AuthenticateMessageParams memory params) internal {
-        bytes memory raw_request = params.serialize();
+        bytes memory raw_request = params.serializeAuthenticateMessageParams();
 
         bytes memory raw_response = Actor.call(AccountTypes.AuthenticateMessageMethodNum, target, raw_request, Misc.CBOR_CODEC, msg.value);
 
@@ -47,7 +45,7 @@ library AccountAPI {
     /// @notice TODO fill this a proper description
     /// @param target The account address (filecoin bytes format) you want to interact with
     function universalReceiverHook(bytes memory target, AccountTypes.UniversalReceiverParams memory params) internal {
-        bytes memory raw_request = params.serialize();
+        bytes memory raw_request = params.serializeUniversalReceiverParams();
 
         bytes memory raw_response = Actor.call(
             AccountTypes.UniversalReceiverHookMethodNum,
