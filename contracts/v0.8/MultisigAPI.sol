873344f4d0cc6c2f6c4d9d2c7103c7308911b46c
Emmanuel <emmanuelm41@gmail.com>
2023-01-30 12:18:54 -0300

    fix; change solc version to specific version

--- contracts/v0.8/MultisigAPI.sol
+++ contracts/v0.8/MultisigAPI.sol
@@ -17,7 +17,7 @@
 // DRAFT!! THIS CODE HAS NOT BEEN AUDITED - USE ONLY FOR PROTOTYPING
 
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity >=0.4.25 <=0.8.17;
+pragma solidity ^0.8.17;
 
 import "./types/MultisigTypes.sol";
 import "./cbor/MultisigCbor.sol";
