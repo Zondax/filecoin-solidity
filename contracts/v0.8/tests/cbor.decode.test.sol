f66d557483d599c3a9a17a69fc93cc9012117777
rllola <rllola80@gmail.com>
2023-02-24 18:51:17 +0100

    some more tests

--- contracts/v0.8/tests/cbor.decode.test.sol
+++ contracts/v0.8/tests/cbor.decode.test.sol
@@ -1,5 +1,5 @@
 /*******************************************************************************
- *   (c) 2022 Zondax AG
+ *   (c) 2023 Zondax AG
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -70,4 +70,87 @@ contract CborDecodeTest {
         (str, index) = input.readString(index);
         require(keccak256(abi.encodePacked(str)) == keccak256(abi.encodePacked("test")), "str is not 'test'");
     }
+
+    function decodeFalse() public {
+        bytes memory input = hex"f4";
+        uint index = 0;
+        bool value;
+
+        (value, index) = input.readBool(0);
+        require(value == false, "value is not false");
+    }
+
+    function decodeTrue() public {
+        bytes memory input = hex"f5";
+        uint index = 0;
+        bool value;
+
+        (value, index) = input.readBool(0);
+        require(value == true, "value is not true");
+    }
+
+    function decodeNull() public {
+        bytes memory input = hex"f6";
+        uint index = 0;
+        bool value;
+
+        value = input.isNullNext(0);
+        require(value == true, "input is not null cbor");
+    }
+
+    function decodeInteger() public {
+        bytes memory input = hex"01";
+        uint index = 0;
+        uint8 value;
+
+        (value, index) = input.readUInt8(0);
+        require(value == 1, "value is not 1");
+    }
+    
+    function decodeString() public {
+        bytes memory input = hex"6a746573742076616c7565";
+        uint index = 0;
+        string memory value;
+
+        (value, index) = input.readString(0);
+        require(value == "test value", "value is not 'test value'");
+    }
+
+    function decodeStringWithWeirdChar() public {
+        bytes memory input = hex"647A6FC3A9";
+        uint index = 0;
+        string memory value;
+
+        (value, index) = input.readString(0);
+        // Does solidity support this ?
+        //require(value == unicode"zoé", "value is not 1");
+    }
+
+    function decodeArrayU8() public {
+        bytes memory input = hex"8501182b184218ea186f";
+
+        uint index = 0;
+        uint arrayLen = 0;
+        uint8 num;
+        string memory str = "";
+
+        (arrayLen, index) = input.readFixedArray(index);
+        require(arrayLen == 5, "array len is not 5");
+
+        (num, index) = input.readUInt8(index);
+        require(num == 1, "num is not 1");
+
+        (num, index) = input.readUInt8(index);
+        require(num == 43, "num is not 43");
+
+        (num, index) = input.readUInt8(index);
+        require(num == 66, "num is not 66");
+
+        (num, index) = input.readUInt8(index);
+        require(num == 234, "num is not 234");
+
+        (num, index) = input.readUInt8(index);
+        require(num == 111, "num is not 111");
+    }
+
 }
