23907aaa8758a60fb6caf7013ad4ed45ef035f92
Emmanuel <emmanuelm41@gmail.com>
2023-03-07 11:46:55 -0300

    feat: use ChainEpoch type on market actor

--- contracts/v0.8/mocks/MarketMockAPI.sol
+++ contracts/v0.8/mocks/MarketMockAPI.sol
@@ -166,13 +166,13 @@ contract MarketMockAPI {
         deal_67.client = 101;
         deal_67.provider = 103;
         deal_67.label = "mAXCg5AIg8YBXbFjtdBy1iZjpDYAwRSt0elGLF5GvTqulEii1VcM";
-        deal_67.start = 25245;
-        deal_67.end = 545150;
+        deal_67.start = CommonTypes.ChainEpoch.wrap(25245);
+        deal_67.end = CommonTypes.ChainEpoch.wrap(545150);
         deal_67.price_per_epoch = 1100000000000;
         deal_67.provider_collateral = 0;
         deal_67.client_collateral = 0;
-        deal_67.activated = 1;
-        deal_67.terminated = 0;
+        deal_67.activated = CommonTypes.ChainEpoch.wrap(1);
+        deal_67.terminated = CommonTypes.ChainEpoch.wrap(0);
 
         deals[deal_67.id] = deal_67;
 
--- contracts/v0.8/mocks/types/MockTypes.sol
+++ contracts/v0.8/mocks/types/MockTypes.sol
@@ -19,6 +19,8 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.17;
 
+import "../../types/CommonTypes.sol";
+
 /// @title Filecoin market actor types for Solidity.
 /// @author Zondax AG
 library MockTypes {
@@ -30,12 +32,12 @@ library MockTypes {
         uint64 client;
         uint64 provider;
         string label;
-        int64 start;
-        int64 end;
+        CommonTypes.ChainEpoch start;
+        CommonTypes.ChainEpoch end;
         uint256 price_per_epoch;
         uint256 provider_collateral;
         uint256 client_collateral;
-        int64 activated;
-        int64 terminated;
+        CommonTypes.ChainEpoch activated;
+        CommonTypes.ChainEpoch terminated;
     }
 }
