ca986df1a13f11d35550b4f155556bffefedc988
swift-mx <huangxinheng@storswift.com>
2023-03-29 10:17:37 +0800

    fix: cyclic dependencie

--- contracts/v0.8/types/CommonTypes.sol
+++ contracts/v0.8/types/CommonTypes.sol
@@ -19,7 +19,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.17;
 
-import "../cbor/BigIntCbor.sol";
 
 /// @title Filecoin actors' common types for Solidity.
 /// @author Zondax AG
